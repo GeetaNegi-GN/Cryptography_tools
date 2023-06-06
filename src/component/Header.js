fa74485d3caf9908f6ce08665f0140472c717c18
Geeta Negi <geetanegi550@gmail.com>
2023-06-06 22:20:34 +0530

    intial commit

---
+++ src/component/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Home from "./Home";
+import Tools from "./Tools";
+import Feedback from "./Feedback";
+
+const Header = () => {
+  return (
+    <div className="navBar">
+      <div className="logo">CryptTools</div>
+      <div className="menu">
+        <ul className="menu-btn">
+          <li>
+            <a href="/">
+              <Home />
+            </a>
+          </li>
+          <li>
+            <a href="/tools">
+              <Tools />
+            </a>
+          </li>
+          <li>
+            <a href="/feedback">
+              <Feedback />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
